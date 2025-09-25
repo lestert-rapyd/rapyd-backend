4003c5318b493117e2ad1b6edf6d337c08536101
lestert-rapyd <lestert@rapyd.net>
2025-09-25 13:45:47 +0800

    Update generate-signature.js

--- utils/generate-signature.js
+++ utils/generate-signature.js
@@ -4,13 +4,15 @@ export function generateRapydSignature(method, urlPath, body) {
   const access_key = process.env.RAPYD_ACCESS_KEY;
   const secret_key = process.env.RAPYD_SECRET_KEY;
   const salt = crypto.randomBytes(8).toString('hex');
-  const timestamp = Math.floor(new Date().getTime() / 1000);
+  const timestamp = Math.floor(Date.now() / 1000);
   const bodyString = body ? JSON.stringify(body) : '';
 
-  const toSign = `${method.toLowerCase()}${urlPath}${salt}${timestamp}${access_key}${secret_key}${bodyString}`;
-  const hash = crypto.createHmac('sha256', secret_key)
+  const toSign = `${method.toUpperCase()}${urlPath}${salt}${timestamp}${access_key}${bodyString}`;
+
+  const signature = crypto
+    .createHmac('sha256', secret_key)
     .update(toSign)
     .digest('base64');
 
-  return { salt, timestamp, signature: hash };
+  return { salt, timestamp, signature };
 }
